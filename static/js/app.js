d25b2e06965a439cd0195fc28e9cbb4766cdb1c3
chrispope12391 <popex107@umn.edu>
2021-04-17 14:48:22 -0500

    4

--- static/js/app.js
+++ static/js/app.js
@@ -7,8 +7,17 @@ function initDashboard() {
     var selector = d3.select("#seldataset");
 
     // comes from office hours with Dom
-    d3.json("data.samples.json").then(function(data) {
+    d3.json("data/samples.json").then(data => {
         console.log(data);
+
+        var sampleNames = data.names;
+
+        sampleNames.forEach(sampleId => {
+            selector.append("option")
+            .text(sampleId)
+            .property("value", sampleId);
+
+        });
     });
 
     //updated the bargraph
